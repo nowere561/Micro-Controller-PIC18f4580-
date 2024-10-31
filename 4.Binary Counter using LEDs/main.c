4a047570ec16e2659fced9402ff622ab80e0441f
nowere561 <nowere241@gmail.com>
2024-10-31 12:07:37 +0530

    changed the names of file and added 4.Binary Counter using LEDs

---
+++ 4.Binary Counter using LEDs/main.c
@@ -0,0 +1,30 @@
+#include <xc.h>
+#define _XTAL_FREQ 8000000
+// CONFIG1H
+#pragma config OSC = HS         // Oscillator Selection bits (HS oscillator)
+#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enable bit (Fail-Safe Clock Monitor disabled)
+#pragma config IESO = OFF       // Internal/External Oscillator Switchover bit (Oscillator Switchover mode disabled)
+
+// Add other necessary configuration bits here
+int leds[]={0x01,0x02,0x04,0x08,0x10,0x20,0x40,0x80};
+int i=0;
+void main(){
+    //Gpio Config
+    TRISD = 0x00;  //output
+   
+
+  
+   
+    while(1)    
+    {
+        PORTD=i;
+        __delay_ms(100);
+        i++;
+        if(PORTD==255){
+            i=0;
+        }
+
+        
+       
+    }
+}
